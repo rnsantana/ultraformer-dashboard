cb95cbfb27cfb698dbf8b878e096536888c5d6a9
Rodrigo Nunes de Santana <97761598+rnsantana@users.noreply.github.com>
2025-04-23 22:13:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 ESSA LINHA corrige o problema de página em branco no Vercel
 });
